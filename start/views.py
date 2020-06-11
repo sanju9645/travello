4b560c2ef2a0d194131fd34e8458506c4bbd0dfe
sanju9645 <sanjusnath9644@gmail.com>
2020-06-11 17:37:56 +0530

    Another commit, created a class called destination in Models.py.., By using this oop concept,set all place properties lik, name, description,price are dynamic while image is still static...and these objects are passed when we call the index function in views.py

--- start/views.py
+++ start/views.py
@@ -5,7 +5,37 @@ from .models import destination
 
 def index(request):
 
-    return render(request,'index.html',{'price': 700})
+    dest1 = destination()
+    dest1.name="Mumbai"
+    dest1.price=800
+    dest1.desc="asdfffgggjjgjgjg"
+
+    dest2 = destination()
+    dest2.name="Calicut"
+    dest2.price=1000
+    dest2.desc="hfuherfuurfugrfrgfrg"
+
+    dest3 = destination()
+    dest3.name="Wayanad"
+    dest3.price=1200
+    dest3.desc="iurfigrfgrfubrubvr"
+
+    dest4 = destination()
+    dest4.name="Kannur"
+    dest4.price=850
+    dest4.desc="asdfffgggrfrfrfrfrfrfrfjjgjgjg"
+
+    dest5 = destination()
+    dest5.name="Malappuram"
+    dest5.price=8000
+    dest5.desc="wsdwwswswswswswswswswsw"
+
+    dest6 = destination()
+    dest6.name="Kottayam"
+    dest6.price=400
+    dest6.desc="asassasasasasasasasasasasasg"
+
+    return render(request,'index.html',{'dest1' : dest1 , 'dest2' : dest2 , 'dest3' : dest3 , 'dest4' : dest4 , 'dest5' : dest5 , 'dest6' : dest6})
 
 def about(request):
     return render(request,'about.html')
