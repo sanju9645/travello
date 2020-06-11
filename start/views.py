f777420e1ba5fd684952fabc4a48c0a9f2c796a7
sanju9645 <sanjusnath9644@gmail.com>
2020-06-11 18:02:59 +0530

    here all objects are passed as list called 'dests' in views.py when calling index function...each place in index page is displayed with the help of this list and for loop..image is also set as dynamic by using a variable 'baseUrl' which represents '/static/images' so we can dynamically access image as <img src = {{baseurl}}/{{dest.img}}

--- start/views.py
+++ start/views.py
@@ -9,33 +9,41 @@ def index(request):
     dest1.name="Mumbai"
     dest1.price=800
     dest1.desc="asdfffgggjjgjgjg"
+    dest1.img="destination_6.jpg"
 
     dest2 = destination()
     dest2.name="Calicut"
     dest2.price=1000
     dest2.desc="hfuherfuurfugrfrgfrg"
+    dest2.img="destination_5.jpg"
 
     dest3 = destination()
     dest3.name="Wayanad"
     dest3.price=1200
     dest3.desc="iurfigrfgrfubrubvr"
+    dest3.img="destination_4.jpg"
 
     dest4 = destination()
     dest4.name="Kannur"
     dest4.price=850
     dest4.desc="asdfffgggrfrfrfrfrfrfrfjjgjgjg"
+    dest4.img="destination_3.jpg"
 
     dest5 = destination()
     dest5.name="Malappuram"
     dest5.price=8000
     dest5.desc="wsdwwswswswswswswswswsw"
+    dest5.img="destination_2.jpg"
 
     dest6 = destination()
     dest6.name="Kottayam"
     dest6.price=400
     dest6.desc="asassasasasasasasasasasasasg"
+    dest6.img="destination_1.jpg"
 
-    return render(request,'index.html',{'dest1' : dest1 , 'dest2' : dest2 , 'dest3' : dest3 , 'dest4' : dest4 , 'dest5' : dest5 , 'dest6' : dest6})
+    dests=[dest1,dest2,dest3,dest4,dest5,dest6]
+
+    return render(request,'index.html',{'dests' : dests})
 
 def about(request):
     return render(request,'about.html')
